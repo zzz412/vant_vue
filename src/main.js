f6a8a3a334525e5eab6f00572a8434450eca7d98
--relace-all <1392187706@qq.com>
2019-12-26 11:59:40 +0800

    init res

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入vant
+import Vant from 'vant';
+// 导入vant的css
+import 'vant/lib/index.css';
+
 Vue.config.productionTip = false
+// 将vant 插件导入到vue实例中
+Vue.use(Vant);
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,45 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Index from '@/views/Index'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    component: Index,
+    children: [
+        {
+          path: '',
+          name: 'home.',
+          //路由懒加载
+          component: () => import('@/views/Home/index')
+        },
+        {
+          path: '/type',
+          name: 'type',
+          //路由懒加载
+          component: () => import('@/views/Type/index')
+        },
+        {
+          path: '/shop',
+          name: 'shop',
+          //路由懒加载
+          component: () => import('@/views/Shop/index')
+        },
+        {
+          path: '/user',
+          name: 'user',
+          //路由懒加载
+          component: () => import('@/views/User/index')
+        }
+    ]
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
